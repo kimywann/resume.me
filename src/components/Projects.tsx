3792cc2acc7e013e6d564fc47c63888f7430d986
kimywann <kimywan10@gmail.com>
2025-08-25 20:51:57 +0900

    contributions 배열 통일

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -5,10 +5,7 @@ interface ProjectsItem {
   deployUrl: string;
   portfolioUrl: string;
   technologies: string[];
-  contributions: {
-    topic: string;
-    details: string[];
-  }[];
+  contributions: string[];
   position: string;
   period: string;
 }
@@ -19,7 +16,7 @@ const projects: ProjectsItem[] = [
     position: "프론트엔드 개발자",
     period: "2025.07.01 - 2025.08.04",
     description:
-      "사용중인 구독 서비스를 등록하고, 월별 지출 흐름을 시각적으로 파악할 수 있는 구독 관리 웹 서비스",
+      "구독 서비스를 많이 이용하다 보니 결제일을 놓치거나, 사용 중인 서비스조차 기억하기 어려운 불편함이 있었습니다. 이를 해결하기 위해, 사용자가 구독 현황과 결제 일정을 한눈에 파악하고 쉽게 관리할 수 있는 웹 서비스를 개발했습니다.",
     githubUrl: "https://github.com/kimywann/wise-sub",
     deployUrl: "https://wise-sub-deploy.vercel.app/",
     portfolioUrl:
@@ -33,27 +30,14 @@ const projects: ProjectsItem[] = [
       "Vercel",
     ],
     contributions: [
-      {
-        topic: "구독 서비스 관리",
-        details: [
-          "Supabase의 RESTful API를 활용해 구독 정보 조회, 등록, 수정, 삭제 기능 구현",
-          "API 호출 시 async/await 기반의 비동기 처리 및 에러 핸들링 적용",
-          "중복 등록 방지를 위해, 이미 등록된 구독 서비스는 UI 상에서 비활성화 처리",
-          "구독 시작일, 결제 주기(월간/연간)에 따라 다음 결제일 표시",
-          "다음 결제일까지 남은 일수를 계산하여 UI 표시",
-          "월간 구독은 매월 비용에 포함, 연간 구독은 시작 월에만 비용 포함되도록 조건부 합산 로직 구현",
-        ],
-      },
-      {
-        topic: "사용자 경험 개선",
-        details: [
-          "특정 월에 활성 상태인 구독만 목록에 표시",
-          "월간 총 구독 금액 및 구독 개수 요약 제공",
-          "CSS Grid를 활용한 반응형 구독 서비스 목록 UI 구현으로 모든 화면 크기에서 일관된 사용자 경험 제공",
-          "조건부 스타일링 (clsx + Tailwind)으로 결제 주기(월간, 연간)에 따른 동적 UI 변화 적용",
-          "useRef를 활용해 로그인, 회원가입 폼 입력 자동 포커스와 에러 시 스크롤 이동을 구현하여 사용자 경험 개선",
-        ],
-      },
+      "Supabase의 RESTful API를 활용해 구독 정보 조회, 등록, 수정, 삭제 기능 구현",
+      "구독 시작일, 결제 주기(월간/연간)에 따라 다음 결제일 표시",
+      "다음 결제일까지 남은 일수를 계산하여 UI 표시",
+      "월간 구독은 매월 비용에 포함, 연간 구독은 시작 월에만 비용 포함되도록 조건부 합산 로직 구현",
+      "특정 월에 활성 상태인 구독만 목록에 표시",
+      "CSS Grid를 활용한 반응형 구독 서비스 목록 UI 구현으로 모든 화면 크기에서 일관된 사용자 경험 제공",
+      "조건부 스타일링 (clsx + Tailwind)으로 동적 UI 변화 구현",
+      "폼 검증 실패 시 useRef를 통한 자동 포커스 이동으로 사용자 입력 오류 최소화",
     ],
   },
 ];
@@ -67,19 +51,21 @@ export default function Experience() {
         </h1>
         <hr className="border-blue-300 w-12 mb-8" />
         <div className="space-y-8">
-          {projects.map((exp, index) => (
+          {projects.map((project, index) => (
             <div key={index}>
               <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between mb-2">
                 <div>
                   <h3 className="text-2xl font-semibold text-gray-900">
-                    {exp.title}
+                    {project.title}
                   </h3>
-                  <p className="text-gray-500 text-sm">{exp.period}</p>
-                  <p className="text-lg text-gray-500">{exp.description}</p>
+                  <p className="text-gray-500 text-sm">{project.period}</p>
+                  <p className="text-md text-gray-600 mt-2 mb-1">
+                    {project.description}
+                  </p>
                 </div>
               </div>
               <div className="flex mb-4">
-                {exp.technologies.map((tech, techIndex) => (
+                {project.technologies.map((tech, techIndex) => (
                   <span
                     key={techIndex}
                     className="text-gray-500 text-sm py-1 px-2 bg-gray-100 rounded-lg mr-2"
@@ -91,7 +77,7 @@ export default function Experience() {
               <div className="flex gap-4 mb-8 text-blue-500">
                 <span>
                   <a
-                    href={exp.githubUrl}
+                    href={project.githubUrl}
                     target="_blank"
                     rel="noopener noreferrer"
                     className="hover:underline"
@@ -101,7 +87,7 @@ export default function Experience() {
                 </span>
                 <span>
                   <a
-                    href={exp.deployUrl}
+                    href={project.deployUrl}
                     target="_blank"
                     rel="noopener noreferrer"
                     className="hover:underline"
@@ -111,7 +97,7 @@ export default function Experience() {
                 </span>
                 <span>
                   <a
-                    href={exp.portfolioUrl}
+                    href={project.portfolioUrl}
                     target="_blank"
                     rel="noopener noreferrer"
                     className="hover:underline"
@@ -122,21 +108,11 @@ export default function Experience() {
               </div>
               <section>
                 <p className="text-md mb-4 font-medium">주요 기여</p>
-                <ul className="space-y-4">
-                  {exp.contributions.map((item, itemIndex) => (
-                    <li key={itemIndex} className="text-gray-700 items-start">
-                      <span className="text-blue-400 text-2xl mr-2">•</span>
-                      <span className="font-semibold">{item.topic}</span>
-
-                      {item.details && (
-                        <ul className="list-[circle] list-inside pl-5 text-md mt-1">
-                          {item.details.map((desc, descIndex) => (
-                            <li className="mt-2" key={descIndex}>
-                              {desc}
-                            </li>
-                          ))}
-                        </ul>
-                      )}
+                <ul className="space-y-1">
+                  {project.contributions.map((item, index) => (
+                    <li key={index} className="text-gray-700 items-start">
+                      <span className="text-2xl mr-2">•</span>
+                      <span>{item}</span>
                     </li>
                   ))}
                 </ul>
